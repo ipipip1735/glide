916276146a617f61d8f1f5059ebfc326e0a644cd
ipipip1735 <ipipip1735@163.com>
2021-03-23 14:26:37 +0800

    Initial commit

---
+++ app/src/main/java/mine/glide/MainActivity.java
@@ -0,0 +1,134 @@
+package mine.glide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by Administrator on 2021/3/23.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_client);
+
+//        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy = new
+//                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+    }
+
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+
+        Glide.with(this)
+                .load("https://cdn2.jianshu.io/shakespeare/_next/static/images/404-203cdc8362d3b571276978d13f5ffd21.png")
+//                .load("http://goo.gl/gEgYUd")
+                .into(imageView);
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+    }
+
+    public void bind(View view) {
+        System.out.println("~~button.bind~~");
+
+    }
+
+    public void unbind(View view) {
+        System.out.println("~~button.unbind~~");
+
+    }
+
+    public void reloading(View view) {
+        System.out.println("~~button.reloading~~");
+
+    }
+
+
+    public void del(View view) {
+        System.out.println("~~button.del~~");
+
+    }
+
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+
+    }
+
+}
